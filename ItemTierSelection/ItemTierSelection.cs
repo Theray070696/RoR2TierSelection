1ee9002fb5f7238b35dfeaa851006797b82fdfcc
Theray070696 <Theray070696@gmail.com>
2019-09-27 10:56:06 -0400

    Initial commit

---
+++ ItemTierSelection/ItemTierSelection.cs
@@ -0,0 +1,109 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+
+namespace Theray070696
+{
+    [BepInPlugin("io.github.Theray070696.itemtierselection", "Item Tier Selection", "1.0.0")]
+    public class ItemTierSelection : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            for(int i = (int)ItemIndex.Syringe; i < (int)ItemIndex.Count; i++)
+            {
+                ItemDef item = ItemCatalog.GetItemDef((ItemIndex) i);
+
+                if(item.tier == ItemTier.NoTier)
+                {
+                    continue;
+                }
+
+                ItemTier currTier = item.tier;
+                int defTier;
+
+                switch(currTier)
+                {
+                    case ItemTier.Tier1:
+                    {
+                        defTier = 1;
+                        break;
+                    }
+
+                    case ItemTier.Tier2:
+                    {
+                        defTier = 2;
+                        break;
+                    }
+
+                    case ItemTier.Tier3:
+                    {
+                        defTier = 3;
+                        break;
+                    }
+
+                    case ItemTier.Lunar:
+                    {
+                        defTier = 4;
+                        break;
+                    }
+
+                    case ItemTier.Boss:
+                    {
+                        defTier = 5;
+                        break;
+                    }
+
+                    default:
+                    {
+                        defTier = 0;
+                        break;
+                    }
+                }
+
+                ConfigWrapper<int> c = Config.Wrap("Item Tiers", item.nameToken + " tier", "Tier of this item. 0 is no tier, 1 is white, 2 is green, 3 is red, 4 is lunar", defTier);
+
+                int newTierNum = c.Value;
+                
+                switch(newTierNum)
+                {
+                    case 1:
+                    {
+                        item.tier = ItemTier.Tier1;
+                        break;
+                    }
+                    
+                    case 2:
+                    {
+                        item.tier = ItemTier.Tier2;
+                        break;
+                    }
+                    
+                    case 3:
+                    {
+                        item.tier = ItemTier.Tier3;
+                        break;
+                    }
+                    
+                    case 4:
+                    {
+                        item.tier = ItemTier.Lunar;
+                        break;
+                    }
+
+                    default:
+                    {
+                        if(newTierNum == 5 && currTier == ItemTier.Boss)
+                        {
+                            break;
+                        }
+                        
+                        item.tier = ItemTier.NoTier;
+                        break;
+                    }
+                }
+            }
+            
+            Logger.LogInfo("Config loaded!");
+        }
+    }
+}
---
+++ ItemTierSelection/bin/Release/netstandard2.0/README.md
@@ -0,0 +1,8 @@
+Since people in the discord loved the other mod I made, I decided to make a better version.
+
+[Discord Screenshot](https://i.imgur.com/RDzDmEp.png)
+
+___
+This mod allows you to set the tier of any non-equipment item in the game. Want Chronobauble to be a white? Go ahead! Want to make every run a broken run? Set 57 Leaf to tier 1, and have a blast! Think Red Whip shouldn't exist (don't see why, it's the best item in the game Kappa)? Have at it!
+
+There is one thing that I'll mention here that isn't mentioned in the config. You might notice that boss items are set to 5 by default in the config. This value only works on boss items, and leaves them as boss items. I didn't allow setting normal items to boss items because I don't know how the game will handle it.
